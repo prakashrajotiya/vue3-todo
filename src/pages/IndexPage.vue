87a8295e5da4afb36117644c2b977a11da050e33
Prakash Kumar <34856687+prakashrajotiya@users.noreply.github.com>
2022-08-04 13:59:43 +0530

    Update IndexPage.vue

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,7 +1,12 @@
 <template>
   <q-page class="q-pa-md" style="padding-top: 74px">
     <todosList />
-    <q-page-sticky expand position="top" class="full-width bg-white">
+    <q-page-sticky
+      expand
+      position="top"
+      class="full-width"
+      :class="$q.dark.isActive ? '' : 'bg-white'"
+    >
       <todosInput />
     </q-page-sticky>
   </q-page>
@@ -11,9 +16,12 @@
 import { defineComponent } from "vue";
 import todosList from "src/components/todos/todoList.vue";
 import todosInput from "src/components/todoInput/todo-input.vue";
+import { useQuasar } from "quasar";
 export default defineComponent({
   name: "IndexPage",
-  setup() {},
+  setup() {
+    const $q = useQuasar();
+  },
   components: {
     todosInput,
     todosList,
